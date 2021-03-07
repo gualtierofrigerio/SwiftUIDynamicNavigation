3efa1705ac908cd0046795f6ae90cb027a07bcbd
Gualtiero <gualtiero.frigerio@gmail.com>
2021-03-07 16:44:02 +0100

    added ViewModels container

--- SwiftUIDynamicNavigation/Coordinator.swift
+++ SwiftUIDynamicNavigation/Coordinator.swift
@@ -13,13 +13,20 @@ enum ViewAction {
     case enableLink
     case dismiss
     case none
+    case refresh
     case showSheet(Bool)
+    case update(ViewState)
 }
 
 /// Coordinator class respnsible for mutating the AppState enum
 /// and provide the correct view for the current state
 class Coordinator:ObservableObject {
     @Published private (set) var state:AppState = .start
+    @Published private (set) var nextAction:ViewAction = .none
+    
+    init() {
+        currentViewModel = viewModel(forState: .start)
+    }
     
     func nextViewModel() -> ContainerViewModel {
         let viewModel = ContainerViewModel(withCoordinator: self)
@@ -29,18 +36,51 @@ class Coordinator:ObservableObject {
         return viewModel
     }
     
-    @Published private var changesPublisher:ViewAction = .none
+    func viewModel(after viewModel:ContainerViewModel) -> ContainerViewModel {
+        if let model = getViewModel(forState: .end) {
+            return model
+        }
+        let viewModel = ContainerViewModel(withCoordinator: self)
+        viewModels.append(ViewModelState(state:.end, viewModel: viewModel))
+        return viewModel
+    }
+    
+    func viewModel(forState state:AppState) -> ContainerViewModel {
+        if let model = getViewModel(forState: state) {
+            return model
+        }
+        let viewModel = ContainerViewModel(withCoordinator: self)
+        viewModels.append(ViewModelState(state:state, viewModel: viewModel))
+        return viewModel
+    }
+    
     private var currentViewModel:ContainerViewModel?
-    private var viewModels:[ContainerViewModel] = []
+    private var viewModels:[ViewModelState] = []
+    
+    private func getViewModel(forState state:AppState) -> ContainerViewModel? {
+        for model in viewModels {
+            if model.state == state {
+                return model.viewModel
+            }
+        }
+        return nil
+    }
     
     
     private func makeCall() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
             if let currentViewModel = self.currentViewModel {
-                print("current view model execute enableLink")
+                print("current view model \(currentViewModel) execute enableLink")
                 currentViewModel.executeViewAction(.enableLink)
             }
             self.state = .success
+            self.currentViewModel?.executeViewAction(.enableLink)
+            self.currentViewModel?.executeViewAction(.update(.start))
+            
+            if let viewModel = self.getViewModel(forState: .end) {
+                viewModel.executeViewAction(.update(.success))
+                self.currentViewModel = viewModel
+            }
         }
     }
 }
@@ -50,13 +90,24 @@ extension Coordinator:ActionsHandler {
         switch action {
         case .cancelCall:
             state = .failure
+            nextAction = .refresh
+            currentViewModel?.executeViewAction(.refresh)
         case .makeCall:
             state = .progress
             makeCall()
+            currentViewModel?.executeViewAction(.update(.progress))
         case .gotoStart:
             state = .start
+            nextAction = .refresh
+            currentViewModel?.executeViewAction(.refresh)
         case .goBack:
-            changesPublisher = .dismiss
+            nextAction = .dismiss
+            currentViewModel?.executeViewAction(.dismiss)
         }
     }
+}
+
+fileprivate struct ViewModelState {
+    var state:AppState
+    var viewModel:ContainerViewModel
 }
